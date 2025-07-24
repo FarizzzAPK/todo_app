002973048c9905961aac11ae70826c6b9dc18a59
Farizzz <113644816+FarizzzAPK@users.noreply.github.com>
2025-07-24 19:22:01 +0300

    Initial commit && splash screen done

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:todo_app/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    double height =MediaQuery.of(context).size.height;
+    double width =MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Container(
+        height: height,
+        width: width,
+        child: SvgPicture.asset(
+          'assets/Splash.svg'
+        )
+      ),
+    );
+  }
+}
